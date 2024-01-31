68748d571152b0510281966d8fd57cb962910e9c
Seok-Hun <hamer1233@naver.com>
2024-01-31 16:57:29 +0900

    Fix: EnableDixcoveryClient 어노테이션 추가

--- src/main/java/kea/dpang/seller/SellerServerApplication.java
+++ src/main/java/kea/dpang/seller/SellerServerApplication.java
@@ -2,8 +2,10 @@ package kea.dpang.seller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SellerServerApplication {
 
     public static void main(String[] args) {
